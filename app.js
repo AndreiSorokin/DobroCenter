3eb46e816668c16b24571a4d35c63ed7c86972f4
Andrei Sorokin <72672144+AndreiSorokin@users.noreply.github.com>
2024-07-03 17:39:26 +0300

    about

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+   $('.header__burger').click(function() {
+      $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+
+   const userLang = navigator.language || navigator.userLanguage;
+   let lang = userLang.startsWith('ru') ? 'ru' : 'fi';
+
+   function loadTranslations() {
+   fetch(`${lang}.json`)
+      .then(response => response.json())
+      .then(translations => {
+         document.querySelectorAll('[data-translate-key]').forEach(element => {
+            const key = element.getAttribute('data-translate-key');
+            if (translations[key]) {
+               element.textContent = translations[key];
+            }
+         });
+      });
+}
+
+   loadTranslations();
+
+   $('#lang-switch').click(function() {
+      lang = lang === 'ru' ? 'fi' : 'ru';
+      loadTranslations();
+   });
+});
